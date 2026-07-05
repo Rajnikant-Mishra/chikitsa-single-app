b50e1f405f847661c4810bd6bd9d7b0fad84563b
prasant-evoquesys <prasantevoquesys@gmail.com>
2026-07-05 15:15:55 +0530

    latest backend

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ import { fileURLToPath } from 'url';
 
 import auth from "./modules/admin/admin.routes.js";
 import Inventory from './modules/inventoryassets/asset.routes.js';
-import rentalRoutes from "./modules/rentalmaster/master.routes.js";
+import rentalRoutes from "./modules/rentalmaster/master.routes.js";  
 
 
 
