bf0e15b30bac8544d313f58a5b3ade737ca18d9e
prasant-evoquesys <prasantevoquesys@gmail.com>
2026-07-29 22:10:02 +0530

    app.js

--- backend/app.js
+++ backend/app.js
@@ -33,6 +33,9 @@ app.use(
 app.use(morgan("dev"));
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "public")));
+
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
